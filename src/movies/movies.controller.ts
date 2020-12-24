aeefd787bc35d6bde69c87a06d6f32a91074f787
cit007 <itcho0077@gmail.com>
2020-12-25 01:01:01 +0900

    transform type

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -32,17 +32,17 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.moviesService.getOne(movieId);
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData) {
     return this.moviesService.udpate(movieId, updateData);
   }
 
   @Delete(':id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -11,7 +11,7 @@ export class MoviesService {
     return this.movies;
   }
 
-  getOne(id: string): Movie {
+  getOne(id: number): Movie {
     const movie = this.movies.find((movie) => movie.id === +id);
     if (!movie) {
       throw new NotFoundException(`movie id${id} is not found`);
@@ -19,7 +19,7 @@ export class MoviesService {
     return movie;
   }
 
-  deleteOne(id: string) {
+  deleteOne(id: number) {
     // getOne for exception message
     this.getOne(id);
     this.movies = this.movies.filter((movie) => movie.id !== +id);
@@ -29,7 +29,7 @@ export class MoviesService {
     this.movies.push({ id: this.movies.length + 1, ...movieData });
   }
 
-  udpate(id: string, updateData) {
+  udpate(id: number, updateData) {
     const movie = this.getOne(id);
     this.deleteOne(id);
     this.create({ ...movie, ...updateData });
