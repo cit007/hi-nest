c9062e4f0dd24811a007b1239bb64bc59aa454ef
cit007 <itcho0077@gmail.com>
2021-01-02 21:04:12 +0900

    post/get from mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MoviesModule, UsersModule],
+  imports: [
+    MoviesModule,
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://<user>:<pass>@cluster0.ei5es.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Model } from 'mongoose';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
-  users: CreateUserDto[] = [];
-  createUser(user: CreateUserDto) {
-    console.log('insert user:', user);
-    this.users.push(user);
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async createUser(user: CreateUserDto) {
+    const createdUser = new this.userModel({
+      username: user.username,
+      password: user.password,
+    });
+    return await createdUser.save();
   }
-  findAll() {
-    return this.users;
+
+  async findAll() {
+    return await this.userModel.find().exec();
   }
 }
