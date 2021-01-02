10f35accdc088eeec1a2050f05741a72083d5de0
cit007 <itcho0077@gmail.com>
2021-01-02 16:38:09 +0900

    class-validator is necessary in dto because using validationpipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUser: CreateUserDto) {
+    console.log('user controller:', createUser);
+    return this.usersService.createUser(createUser);
+  }
+
+  @Get()
+  getUserAll() {
+    return this.usersService.findAll();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users: CreateUserDto[] = [];
+  createUser(user: CreateUserDto) {
+    console.log('insert user:', user);
+    this.users.push(user);
+  }
+  findAll() {
+    return this.users;
+  }
+}
