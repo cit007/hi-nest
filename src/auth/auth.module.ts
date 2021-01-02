831acad41ebeb519f3deb4d7e1291f781dc3c627
cit007 <itcho0077@gmail.com>
2021-01-02 22:03:02 +0900

    import UsersModule for userService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [
+    UsersModule,
     JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1h' } }),
   ],
   controllers: [AuthController],
