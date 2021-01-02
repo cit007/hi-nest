5ce77b25dc3407d88ac7101a5aacc0cac28b3a74
cit007 <itcho0077@gmail.com>
2021-01-02 12:14:33 +0900

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, UsersModule],
   controllers: [],
   providers: [],
 })
